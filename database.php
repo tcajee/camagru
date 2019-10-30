53376c1de9e640a3ade3e569adfb9d65ae794edc
Tameem CAJEE <tcajee@c5r3s4.wethinkcode.co.za>
2019-10-30 17:31:27 +0200

    db ready

--- database.php
+++ database.php
@@ -8,12 +8,12 @@
 
 	try{
 		$conn = new PDO("mysql:hostname=$hostname", $username, $password);
-
-		$db = $conn->prepare($drop_database);
+        
+        $db = $conn->prepare($drop_database);
 		$db->execute();
 		$db = $conn->prepare($create_database);
 		$db->execute();
-
+        
         $conn = new PDO("mysql:hostname=$hostname;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch(PDOException $e){

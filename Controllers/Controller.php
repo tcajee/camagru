5a4aa4b563a10f268a0532f0decf2afa6f63e1cc
Tameem Cajee <tcajee@student.wethinkcode.co.za>
2019-11-07 13:08:44 +0200

    restructured mvc and fixed bugs

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -4,12 +4,12 @@ class Controller extends Database {
 
 	/* public function view($view, $data = []) { */
     public static function view($view) {
-        require_once './Views/' . $view . '.php';
+        require_once("./Views/$view.php");
     }
 	
      public function model($model) {
 
-		require_once './Models/' . $model . '.php';
+        require_once("./Models/$model.php");
 		return new $model();
 	 }
 }

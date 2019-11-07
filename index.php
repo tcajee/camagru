a6d2d3ab8172c8b918cdbf94d735caad081f5739
Tameem Cajee <tcajee@student.wethinkcode.co.za>
2019-11-07 16:33:39 +0200

    restructuring mvc... again

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
+
 <?php
 
+    session_start();
+   
+    define('DS', DIRECTORY_SEPARATOR);
+    define('ROOT', dirname(__FILE__));
+   
+    $url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : [];    
+	// $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+    // var_dump($url);
+    // if (isset($_SERVER['PATH_INFO'])) {
+    //    echo $_SERVER['PATH_INFO'];
+    // }
+    // echo $_SERVER['REQUEST_URL'];
 
-spl_autoload_register(function($class_name) {
-    if (file_exists('./classes/' . $class_name . '.php'))
-        require_once("./classes/$class_name.php");
-    else if (file_exists('./Controllers/' . $class_name . '.php'))
-        require_once("./Controllers/$class_name.php");
-});
+    require_once(ROOT . DS . 'core' . DS . 'bootstrap.php');
+    
+    
+    
+    
 
-require_once("./config/routes.php");
+// spl_autoload_register(function($class_name) {
+//     if (file_exists('./classes/' . $class_name . '.php'))
+//         require_once("./classes/$class_name.php");
+//     else if (file_exists('./Controllers/' . $class_name . '.php'))
+//         require_once("./Controllers/$class_name.php");
+// });
+
+// require_once("./config/routes.php");

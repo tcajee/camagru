3079a605aa32a9b08869fe9af6108ca025305cbf
Tameem Cajee <tcajee@student.wethinkcode.co.za>
2019-11-11 11:34:50 +0200

    added bootstrap.php configired config, router and helpers

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
-
 <?php
 
-    session_start();
+session_start();
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__FILE__));
    
-    define('DS', DIRECTORY_SEPARATOR);
-    define('ROOT', dirname(__FILE__));
-   
-    $url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : [];    
-	// $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+$url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : [];    
+
+require_once(ROOT . DS . 'core' . DS . 'bootstrap.php');
+
+    
+    
+    // $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
     // var_dump($url);
     // if (isset($_SERVER['PATH_INFO'])) {
     //    echo $_SERVER['PATH_INFO'];
     // }
     // echo $_SERVER['REQUEST_URL'];
 
-    require_once(ROOT . DS . 'core' . DS . 'bootstrap.php');
-    
-    
-    
     
 
 // spl_autoload_register(function($class_name) {
@@ -28,3 +28,4 @@
 // });
 
 // require_once("./config/routes.php");
+// require_once("./config/setup.php");

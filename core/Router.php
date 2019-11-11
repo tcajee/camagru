d2af9cbaa4577138cf509bf4209dfb8c27094390
Tameem Cajee <tcajee@student.wethinkcode.co.za>
2019-11-11 12:23:47 +0200

    added route functionlity (incomplete)

--- core/Router.php
+++ core/Router.php
@@ -3,6 +3,30 @@
 class Router {
     
     public static function route($url) {
-        dnd($url);
+
+        // Controller
+        $controller = (isset($url[0]) && $url[0] != '') ? ucwords($url[0]) : DEF_CONTROLLER;
+        $controller_name = $controller;
+        array_shift($url);
+        
+        // Action
+        $action = (isset($url[0]) && $url[0] != '') ? $url[0] . 'Action' : 'indexAction';
+        $action_name = $controller;
+        array_shift($url);
+    
+        // Paramaters
+        $queryParams = $url;
+
+        $dispatch = new $controller($controller_name, $action);
+
+        if (method_exists($controller, $action)) {
+            call_user_func_array([$dispatch, $action], $queryParams);
+        } else {
+            // die("method $action does not exist in $controller");
+            die('That method does not exist in \"' . $controller . '\"');
+        }
     }
+
+
+
 }

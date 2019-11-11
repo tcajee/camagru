3c2a21b3ebc6337ac790a0691697f20c1800bf56
Tameem Cajee <tcajee@student.wethinkcode.co.za>
2019-11-11 14:57:46 +0200

    added Home controller and views

---
+++ app/controllers/temp/Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+class Controller extends Database {
+
+    /* public function view($view, $data = []) { */
+        
+ public function __construct($controller, $action) {
+        parent::__construct($controller, $action);
+    }
+
+    public function indexAction() {
+        die('Welcome to Home/index');
+    }
+
+}

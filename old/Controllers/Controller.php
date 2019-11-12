72177577c8aa39e462ab58dd8e62a7bcbb46e77a
Tameem Cajee <tcajee@student.wethinkcode.co.za>
2019-11-12 15:01:56 +0200

    moved old

--- old/Controllers/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Controller extends Database {
-
-    public static function CreateView($viewName) {
-        require_once("./Views/$viewName.php");
-    }
-}
-?>
--- old/pages/forgot_mail.php
+++
@@ -1,7 +0,0 @@
-//Find user with this email and send them a link with their user and hashed password to the pass_reset.php
-<?php
-
-//dlfkj
-
-
-?>

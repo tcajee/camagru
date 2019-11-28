8675897191ee69c14a0bc4f0658733a9a4554fe4
sminnaar <sminnaar@student.wethinkcode.co.za>
2019-11-28 15:06:50 +0200

    Added profile pics: need to link them to database, added sticker button: need to write function and link them to database

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -2,7 +2,7 @@
   <div class="bar black card">
   <a href="#" class="bar-item button padding-large">PROFILE</a>
   <a href="#" class="bar-item button padding-large">SETTINGS</a>
-  <a href="#" class="bar-item button padding-large">UPLOAD</a>
+  <a href="upload" class="bar-item button padding-large">UPLOAD</a>
 	<a href="#" class="bar-item button padding-large">LOGIN/LOGOUT</a>
   </div>
 </div>

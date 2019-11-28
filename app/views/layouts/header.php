811157e3c11a9fd48d997a1910d576fe35a1c7eb
sminnaar <sminnaar@student.wethinkcode.co.za>
2019-11-28 15:21:06 +0200

    Added profile link on banner(for testing)

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -1,8 +1,8 @@
-<div class="center">
-  <div class="bar black card">
-  <a href="#" class="bar-item button padding-large">PROFILE</a>
-  <a href="#" class="bar-item button padding-large">SETTINGS</a>
-  <a href="upload" class="bar-item button padding-large">UPLOAD</a>
-	<a href="#" class="bar-item button padding-large">LOGIN/LOGOUT</a>
+  <div class="center">
+    <div class="bar black card">
+    <a href="profile" class="bar-item button padding-large">PROFILE</a>
+    <a href="#" class="bar-item button padding-large">SETTINGS</a>
+    <a href="upload" class="bar-item button padding-large">UPLOAD</a>
+	  <a href="#" class="bar-item button padding-large">LOGIN/LOGOUT</a>
+    </div>
   </div>
-</div>

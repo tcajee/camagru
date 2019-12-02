17512ff36782aec18d91e6b98f14b0b6fb2e141c
sminnaar <sminnaar@student.wethinkcode.co.za>
2019-12-02 18:21:38 +0200

    icons done

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -3,11 +3,11 @@
     echo  "<div class='center'>
     <hr>
     <div class='bar black card'>
-      <a href='profile' class='bar-item button padding-small'><img style='width: 20px' src='img/profile/def4.jpg'></a>
-      <a href='settings' class='bar-item button padding-small'><img style='width: 25px' src='img/icons/settings.png'></a>
-      <a href='#' class='bar-item button padding-small'><img style='width: 25px' src='img/icons/Gallery.png'></a>
-      <a href='upload' class='bar-item button padding-small'><img style='width: 25px' src='img/icons/Upload.png'></a>
-      <a href='logout' class='bar-item button padding-small'><img style='width: 25px' src='img/icons/Power.png'></a>
+      <a href='profile' class='bar-item button padding-small'><img style='width: 50px; margin-right: 35px' src='img/profile/def4.jpg'></a>
+      <a href='settings' class='bar-item button padding-small'><img style='width: 53px; margin-right: 35px' src='img/icons/settings.png'></a>
+      <a href='#' class='bar-item button padding-small'><img style='width: 50px; margin-right: 35px' src='img/icons/Files.png'></a>
+      <a href='upload' class='bar-item button padding-small'><img style='width: 55px; margin-right: 35px' src='img/icons/Camera.png'></a>
+      <a href='logout' class='bar-item button padding-small'><img style='width: 53px' src='img/icons/Exit.png'></a>
     </div>
     <hr>
     </div>";

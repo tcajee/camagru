c064a071599e2f785f17419a674f31615c3ff460
sminnaar <sminnaar@student.wethinkcode.co.za>
2019-12-03 12:39:31 +0200

    Settings

--- app/controllers/Settings.php
+++ app/controllers/Settings.php
@@ -2,7 +2,14 @@
 
 class Settings extends Controller {
  
+    public function __construct($controller, $action) {
+        parent::__construct($controller, $action);
+        $this->_db = DB::getInstance();
+        var_dump($this);
+    }
+
     public function index() {
     $this->view->render('settings');
     }
+    
 }
--- app/views/settings.php
+++ app/views/settings.php
@@ -7,7 +7,7 @@
     <input id="lname" type="text" name="lname" value="" placeholder="Last Name"><p></p>
     <input id="submit" type="button" name="Submit" value="Submit" /><p></p>
     <br />
-    <input id="upload_ppic" type="button" name="upload" value="Upload Profile Picture" /><p></p>
+    <input id="upload_ppic" type="button" name="upload_p" value="Upload Profile Picture" /><p></p>
     <input id="change_p" type="button" name="change_password" value="Change Password" /><p></p>
     <input id="change_e" type="button" name="change_email" value="Change Email" /><p></p>
 </div>

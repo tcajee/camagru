9db8a6a0ef72b8f745224acc18ab645f5991a6be
Tameem CAJEE <tcajee@c5r3s4.wethinkcode.co.za>
2019-12-10 14:16:53 +0200

    gallery fully functional and added like counts and column display

--- app/controllers/Gallery.php
+++ app/controllers/Gallery.php
@@ -3,10 +3,8 @@
 class Gallery extends Controller {
     
     public $_db;
-   
-    // public $all = [];
     public $images;
-    public $ret = 0;
+    public $comments;
     public $n = 0;
     public $start = [];
     public $next = [];
@@ -19,20 +17,70 @@ class Gallery extends Controller {
 
     public function setup() {
         $images = [];
+        $comments = [];
         $this->images = $this->_db->query('SELECT * FROM posts')->results();
+        $this->comments = $this->_db->query('SELECT * FROM comments')->results();
+        // dnd($this->comments);
         foreach ($this->images as $img) {
-            $images[] = ['image'=>$img->img, 'likes'=>$img->likes]; 
+            // dnd($img);
+            foreach ($this->comments as $comment) {
+                // dnd($comment);
+                // $comments[] = 'dummy';
+                if ($comment->post == $img->id) {
+                    $comments[] = $comment->text;
+                }
+            }
+            $images[] = [
+                'image' => $img->img,
+                'likes' => $img->likes,
+                'comments' => $comments
+            ];
+            $comments = [];
         }
+        // dnd($images);
         $this->n = count($images);
         $i = $_POST['count'];
         $count = 5;
+        $start = 1;
         if (isset($_POST['start']) && $_POST['start']) {
             
             while ($i < $this->n - 1 && $count) {
-                echo "<img src='" . $images[$i]['image'] . "' style='width: 25%'><p></p>";
-                echo "<input class='button text-black grey' id='like' name='next' type='submit' value='Like'/>";
-                echo "<p> Likes: " . $images[$i]['likes'] . "</p>";
-                echo "<p>comments: </p>";
+                echo "<div id='imagess'>";
+                    echo "<img src='" . $images[$i]['image'] . "' style='width: 25%'><p></p>";
+                echo "</div>";
+                echo "<div id='likes'>";
+                    echo "<input class='button text-black grey' id='like' name='next' type='submit' value='Like'/>";
+                    echo "<p> Likes: " . $images[$i]['likes'] . "</p>";
+                echo "</div>";
+                if ($images[$i]['comments']) {
+                    echo "<div style='display: inline-flex'>";
+                        echo "<a class='prev' onclick='plusSlides(-1)'>&#10094; Prev</a>";
+                    echo "</div>";
+                    echo "<div id='comments' class='slideshow-container' style='display: inline-flex'>";
+                    foreach ($images[$i]['comments'] as $text) {
+                        if ($start) {
+                            echo "<div class='comments fade center' style='display: block'>";
+                                echo "<p>" . $text . " </p>";
+                            echo "</div>";
+                        } else {
+                            echo "<div class='comments fade center' style='display: none'>";
+                                echo "<p>" . $text . " </p>";
+                            echo "</div>";
+                        } 
+                        $start = 0;
+                    }
+                    echo "</div>";
+                    echo "<div style='display: inline-flex'>";
+                            echo "<a class='next' onclick='plusSlides(1)'>Next &#10095;</a>";
+                        echo "</div>";
+                    echo "<div class='padding-16'>";
+                        echo "<a class='prev' onclick='allSlides()'> All Comments </a>";
+                    echo "</div>";
+                } else {
+                    echo "<div class='center'>";
+                    echo "<p> No comments</p>";
+                    echo "</div>";
+                }
                 $i++;
                 $count--;
             }
@@ -40,12 +88,13 @@ class Gallery extends Controller {
         }
     } 
 
-    // public function display($imgs = [], $likes, $comments = []) {
     public function display() {
         $images = [];
+        $comments = [];
+        $this->images = $this->_db->query('SELECT * FROM posts')->results();
         $this->images = $this->_db->query('SELECT * FROM posts')->results();
         foreach ($this->images as $img) {
-            $images[] = $img->img; 
+            $images[] = ['image'=>$img->img, 'likes'=>$img->likes]; 
         }
         $this->n = count($images);
         $i = $_POST['count'];
--- js/gallery.js
+++ js/gallery.js
@@ -6,6 +6,8 @@
         let count = 0;
        
         const gallery = document.getElementById("gallery");
+        const likes = document.getElementById("likes");
+        const comments = document.getElementById("comments");
         let prevbutton = document.getElementById("prev");
         let nextbutton = document.getElementById("next");
         prevbutton.onclick = prev;
@@ -18,13 +20,25 @@
                 if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
                     resData = res.target.response;
                     if (resData) {
+                      
+                        gallery.innerHTML = resData;
+
+                        var like = document.getElementById("likes");
+                        likes.innerHTML = '';
+                        likes.innerHTML = like;
+                        
+                        var comment = document.getElementById("comments");
+                        comments.innerHTML = '';
+                        comments.innerHTML = comment;
+
+                        // resData.removeChild('likes'); 
+                        // resData.removeChild('comments'); 
                         gallery.innerHTML = '';
                         gallery.innerHTML = resData;
+
                         var counter = document.getElementById("counter");
                         count = Number(counter.innerHTML);
-
                         console.log('count' + count);
-
                         loaded = 1;
                     } else {
                         window.location.assign('gallery');

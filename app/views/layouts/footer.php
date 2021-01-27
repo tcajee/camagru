422976878997c14ffec139c8db701fa9f2934861
sminnaar <sminnaar@student.wethinkcode.co.za>
2021-01-27 10:32:30 +0200

    Removed a ferw redundant things

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,5 +1,14 @@
 <div class="center">
   <hr>
-    <a href="https://github.com/tcajee"><p class="text-grey small">tcajee</a> & <a href="https://github.com/sminnaar">sminnaar</a></p>
+    <p class="text-grey small">tcajee
+      |  
+     sminnaar
+      | 
+     vscott
+      | 
+     lpetsoane
+      | 
+     bnkosi
+     </p>
   <hr>
 </div>

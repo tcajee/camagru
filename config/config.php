8b0fbf5bb005a7cda597e6204ab2a434737b9ea3
Bongani Nkosi <dbongs@gmail.com>
2021-01-30 09:16:15 +0200

    added comment

--- config/config.php
+++ config/config.php
@@ -20,3 +20,4 @@ define('SITE_TITLE', 'Camagru');
 define('SESSION_NAME', 'KJ4DSHj4kd9sfh3J2DFBrusbFg');
 define('REMEMBER_ME', 'vPI1PAJD2F0bskjdvO97IK3A5sjf');
 define('REMEMBER_ME_EXPIRY', 86400);
+//update config
